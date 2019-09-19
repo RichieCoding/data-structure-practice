9fa320a8716c934a0c9924c7b5ee016aabfd3730
Richard <richardli125@gmail.com>
2019-09-19 19:56:52 -0400

    added singly linked list

---
+++ Singly Linked List/singly-linked-list.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(value) {
+    this.vaule = value
+    this.next = null
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  push(value) {
+    let newNode = new Node(value)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+    this.length++
+    return this;
+  }
+
+
+}
+
