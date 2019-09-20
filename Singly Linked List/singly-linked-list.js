be0c8c1a440fa8527390c6b9495cbff6038cee85
Richard <richardli125@gmail.com>
2019-09-19 23:19:08 -0400

    added pop method

--- Singly Linked List/singly-linked-list.js
+++ Singly Linked List/singly-linked-list.js
@@ -13,16 +13,44 @@ class SinglyLinkedList {
   }
 
   push(value) {
+    // First we'll declare a newNode variable and create a new Node instance
     let newNode = new Node(value)
+    // Check if this the head is null then set head and tail to our first Node
     if (!this.head) {
       this.head = newNode
       this.tail = newNode
     } else {
+    // If the head is not null then set the current tail.next to our newNode that we just passed in. Then we'll set our newNode as the current tail 
       this.tail.next = newNode
       this.tail = newNode
     }
     this.length++
     return this;
+  }
+
+  pop() {
+    // Checks to see if head is null, if so return undefined
+    if (!this.head) return undefined
+    // Declare two variables to keep track of our tail
+    let current = this.head
+    let newTail = this.head
+    // Loop through as long as current if not null and set our newTail to the current while it being behind the current by one
+    while(current) {
+      newTail = current
+      current = current.next
+    }
+    // Set our tail to be the newTail which should be the second to last node then set the tail.next to point to nothing which will be null
+    this.tail = newTail
+    this.tail.next = null
+    // Checks to see if its the last node in our list and if so put head and tail to null
+    if (this.length === 0) {
+      this.head = null
+      this.tail = null
+    }
+    // Decrement the length each time we call pop()
+    this.length--
+    // Return our current Node which after the while loop should have been the last Node that we began with
+    return current
   }
 
 
